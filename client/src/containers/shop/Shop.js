8ada5ea0ae39129b8e32c2e1ca4e0dba6f8b7009
Matthew Bower <mttbwr91@gmail.com>
2020-05-05 15:09:24 -0700

    start of 135

--- client/src/containers/shop/Shop.js
+++ client/src/containers/shop/Shop.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Route } from 'react-router-dom';
 import './Shop.styles.scss';
+// containers
+import Category from '../category/Category';
 // components
 import CollectionOverview from '../../components/collection-overview/CollectionOverview';
 import Header from '../../components/header/Header';
 
-const Shop = ({ collections }) => {
+// shop is a nested route in app.js so we have access to these props
+const Shop = ({ match }) => {
   return (
     <div>
       <Header />
       <div className="shop-page">
-        <CollectionOverview />
+        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route path={`${match.path}/:categoryId`} component={Category} />
       </div>
     </div>
   )
