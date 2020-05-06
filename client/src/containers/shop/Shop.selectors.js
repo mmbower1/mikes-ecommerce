3898a04ccbe3ba586bffdf20d688224e7d13a444
Matthew Bower <mttbwr91@gmail.com>
2020-05-05 18:50:36 -0700

    fix collections-overview map error

--- client/src/containers/shop/Shop.selectors.js
+++ client/src/containers/shop/Shop.selectors.js
@@ -1,5 +1,15 @@
 import { createSelector } from 'reselect';
 
+const COLLECTION_ID_MAP = {
+  tinctures: 1,
+  edibles: 2,
+  topicals: 3,
+  capsules: 4,
+  pets: 5,
+  sleep: 6,
+  bundles: 7
+};
+
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -11,5 +21,7 @@ export const selectCollections = createSelector(
 export const selectCollection = collectionUrlParam => 
   createSelector(
     [selectCollections],
-    collections => collections[collectionUrlParam]
+    collections => collections.find(collection => 
+      collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+    )
   );
