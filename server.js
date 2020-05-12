8922c671bac44adb99805fe6388dd75acca4eae6
Matthew Bower <mttbwr91@gmail.com>
2020-05-12 11:40:47 -0700

    push to heroku test

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const path = require('path');
 var PORT = process.env.PORT || 5000;
 dotenv.config({ path: './config/config.env' });
 
-
 // connect mongo
 connectDB();
 
@@ -37,7 +36,7 @@ app.use('/stripe', require('./routes/stripe'));
 
 // heroku 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static("client/build"));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
