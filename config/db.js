8bd7d418001fffb6901205a69597bae64fd63986
Matthew Bower <mttbwr91@gmail.com>
2020-05-12 13:37:02 -0700

    mlab uri

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://localhost:27017/mikes-ecommerce' || process.env.MONGODB_URI, {
+    const conn = await mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds155424.mlab.com:55424/heroku_sjx4dl0r' || process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false,
